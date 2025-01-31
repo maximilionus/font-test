ddee92d501ba617f522e989735ee45d95f27914b
maximilionus <maximilionuss@gmail.com>
2025-01-31 04:00:27 +0300

    Use panagrams as samples, switch to GPLv3

--- scripts/generate_content.js
+++ scripts/generate_content.js
@@ -1,5 +1,24 @@
+/*********************************************************************
+Script for filling the index page with content from sample.
+Copyright (C) 2024-2025  Max Gashutin <maximilionuss@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*********************************************************************/
+
+
 const sourceText = document.getElementById('text-source').innerHTML
-const fontSizes = [6, 7, 8, 10, 12, 14, 18, 20, 24, 48];
+const fontSizes = [6, 7, 8, 10, 12, 14, 18, 24];
 
 document.querySelectorAll(
     '.font-sans-serif, .font-serif, .font-system-ui'
